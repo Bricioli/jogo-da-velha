fc10d03bd03d79309b033b023d63c7599993abd0
Jared Ricioli <jared.ricioli@live.com>
2022-03-12 19:40:01 -0300

    Adicionado a view as ações do jogo

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JogoDaVelhaService } from './shared';
 
 @Component({
   selector: 'app-jogo-da-velha',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
 
   ngOnInit(): void {
+    this.jogoDaVelhaService.initialize();
   }
+
+  get showBegin(): boolean {
+    return this.jogoDaVelhaService.showBegin;
+  }
+
+  get showBoard(): boolean {
+    return this.jogoDaVelhaService.showBoard;
+  }
+  get showEnd(): boolean {
+    return this.jogoDaVelhaService.showEnd;
+  }
+  gameStart(): void {
+    this.jogoDaVelhaService.gameStart();
+  }
+
+  play(posX: number, posY: number) : void {
+    this.jogoDaVelhaService.play(posX, posY);
+  }
+  showExis(posX: number, posY: number) : boolean {
+    return this.jogoDaVelhaService.showExis(posX, posY);
+  }
+  showBall(posX: number, posY: number) : boolean {
+    return this.jogoDaVelhaService.showBall(posX, posY);
+  }
+  showWin(posX: number, posY: number) : boolean {
+    return this.jogoDaVelhaService.showWin(posX, posY);
+  }
+  
 
 }
